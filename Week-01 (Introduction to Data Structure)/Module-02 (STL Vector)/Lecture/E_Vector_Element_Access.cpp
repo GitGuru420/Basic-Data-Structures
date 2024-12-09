8303824f04ceb2ed5a222b04078261d24c4bc245
GitGuru420 <raisulislam1892@gmail.com>
2024-12-09 21:30:30 +0530

    Vector Element Access

---
+++ Week-01 (Introduction to Data Structure)/Module-02 (STL Vector)/Lecture/E_Vector_Element_Access.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> v = {1,2,3,2,5,4,7,2};
+    cout << v[2] << endl;
+    cout << v.back() << endl;
+    cout << v.front() << endl;
+
+    return 0;
+}
