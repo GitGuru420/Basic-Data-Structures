16e434598aae8ea863e052aee3c6afd02489991c
GitGuru420 <raisulislam1892@gmail.com>
2024-12-09 13:13:01 +0530

    Space

---
+++ Week-01 (Introduction to Data Structure)/Module-01 (Time and Space Complexity)/Phitron-Book/E_Space_Complexity.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    // example-1
+    /*
+    int result = 0;
+    for(int i=0; i<n; i++) {
+        result += i;
+    }
+    // here is no new variable to get in, so space complexity: O(1)
+    */
+
+    // example-2
+    int ar[n];  // space complexity: O(N)
+    for(int i=0; i<n; i++) {
+        cout << ar[i] << " ";
+    }
+
+    return 0;
+}
