40f731e436a36445d14de49f9196412a1ec50834
GitGuru420 <raisulislam1892@gmail.com>
2024-12-10 12:04:18 +0530

    New Array

---
+++ Week-01 (Introduction to Data Structure)/Module-02 (STL Vector)/Practice/Practice_Problem_1.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int i=0; i<n; i++) {
+        cin >> a[i];
+    }
+    vector<int> b(n);
+    for(int i=0; i<n; i++) {
+        cin >> b[i];
+    }
+    a.insert(a.begin(), b.begin(), b.end());
+    for(int x:a) {
+        cout << x << " ";
+    }
+
+    return 0;
+}
