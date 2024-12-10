3ed494092d947293a701637722c7cb01ce2b87d7
GitGuru420 <raisulislam1892@gmail.com>
2024-12-10 15:18:50 +0530

    Vector modifier function

---
+++ Week-01 (Introduction to Data Structure)/Module-02 (STL Vector)/Phitron-Book/C_Vector_Modifier_Function.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // vector assign
+    vector<int> v = {1,2,3,4,5};
+    // vector<int> v1;
+    // v1 = v;
+
+    // push_back()
+    // v.push_back(6);
+    // for(int i=0; i<v.size(); i++) {
+    //     cout << v[i] << " ";
+    // }
+
+    // pop_back()
+    // v.pop_back();
+    // for(int i=0; i<v.size(); i++) {
+    //     cout << v[i] << " ";
+    // }
+
+    // insert()
+    // v.insert(v.begin()+2, 10);
+    // for(int i=0; i<v.size(); i++) {
+    //     cout << v[i] << " ";
+    // }
+    // vector<int> v2 = {90, 99};
+    // v.insert(v.begin()+3, v2.begin(), v2.end());
+    // for(int i=0; i<v.size(); i++) {
+    //     cout << v[i] << " ";
+    // }
+
+    // erase()
+    // v.erase(v.begin()+2);
+    // v.erase(v.begin()+1, v.begin()+3);
+    // for(int i=0; i<v.size(); i++) {
+    //     cout << v[i] << " ";
+    // }
+
+    return 0;
+}
