11ff7f359871e09058cb00c479eb3fc5ee0dac11
GitGuru420 <raisulislam1892@gmail.com>
2024-12-11 18:49:10 +0530

    Time Complexity

--- Week-01 (Introduction to Data Structure)/Module-2.5 (Practice Day - 01)/Problem2.cpp
+++ Week-01 (Introduction to Data Structure)/Module-2.5 (Practice Day - 01)/Problem2.cpp
@@ -8,19 +8,19 @@ int main() {
     for(int i=0; i<n; i++) {
         cin >> a[i];
     }
-    for(int i=0; i<n; i++) {
+    for(int i=0; i<n; i++) {    // O(n)
         if(a[i] > 0) {
-            replace(a.begin(), a.end(), a[i], 1);
+            replace(a.begin(), a.end(), a[i], 1);   // O(n)
         }
         else if(a[i] < 0) {
-            replace(a.begin(), a.end(), a[i], 2);
+            replace(a.begin(), a.end(), a[i], 2);   // O(n)
         }
     }
     for(int x:a) {
         cout << x << " ";
     }
 
-    // time complexity: O(n)
+    // time complexity: O(n*n)
 
     return 0;
 }
