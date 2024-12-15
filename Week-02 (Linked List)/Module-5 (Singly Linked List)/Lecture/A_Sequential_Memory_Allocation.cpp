b73cd926b5fa9af63feab53061dcbb6664a8ed38
GitGuru420 <raisulislam1892@gmail.com>
2024-12-15 07:25:11 +0530

    Sequential memory allocation

---
+++ Week-02 (Linked List)/Module-5 (Singly Linked List)/Lecture/A_Sequential_Memory_Allocation.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // int ar[5] = {1,2,3,4,5};
+    // cout << &ar[2] << " " << &ar[3] << endl;
+    // cout << (int)&ar[2] << " " << (int)&ar[3] << endl;
+
+    // vector<int> v = {1,2,3,4,5};
+    // cout << (int)&v[2] << " " << (int)&v[3] << endl;
+
+    return 0;
+}
