024f45ca4914d944c4c28cf53cb2c2cb5763da6e
GitGuru420 <raisulislam1892@gmail.com>
2024-12-16 01:25:10 +0530

    Sequential Memory Allocation

--- Week-02 (Linked List)/Module-5 (Singly Linked List)/Lecture/A_Sequential_Memory_Allocation.cpp
+++ Week-02 (Linked List)/Module-5 (Singly Linked List)/Lecture/A_Sequential_Memory_Allocation.cpp
@@ -1,13 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    // int ar[5] = {1,2,3,4,5};
-    // cout << &ar[2] << " " << &ar[3] << endl;
-    // cout << (int)&ar[2] << " " << (int)&ar[3] << endl;
+int main()
+{
+    int arr[5] = {1, 2, 3, 4, 5};
+    // cout << arr[2] << " " << arr[3] << endl;
+    // cout << &arr[1] << " " << &arr[2] << endl;
+    // cout << (int)&arr[1] << " " << (int)&arr[2] << endl;    // this line give me error, because my windows
+    cout << (uintptr_t)&arr[1] << " " << (uintptr_t)&arr[2] << endl;
 
-    // vector<int> v = {1,2,3,4,5};
-    // cout << (int)&v[2] << " " << (int)&v[3] << endl;
+    // vector
+    vector<int> v = {1, 2, 3, 4, 5};
+    // cout << v[3] << " " << v[4] << endl;
+    cout << (uintptr_t)&v[2] << " " << (uintptr_t)&v[3] << " " <<  (uintptr_t)&v[4] << endl;
 
     return 0;
 }
