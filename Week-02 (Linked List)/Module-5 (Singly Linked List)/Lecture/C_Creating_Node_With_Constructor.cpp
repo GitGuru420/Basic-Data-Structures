373755ab9e8e5124f86df28e744f2df77fb339d7
GitGuru420 <raisulislam1892@gmail.com>
2024-12-16 12:42:07 +0530

    Creating Node with Constructor

--- Week-02 (Linked List)/Module-5 (Singly Linked List)/Lecture/C_Creating_Node_With_Constructor.cpp
+++ Week-02 (Linked List)/Module-5 (Singly Linked List)/Lecture/C_Creating_Node_With_Constructor.cpp
@@ -1,26 +1,32 @@
 #include <bits/stdc++.h>
-using namespace std; 
+using namespace std;
 
-class Node {
-    public:
-        int val;
-        Node* next;
+class Node
+{
+public:
+    int val;
+    Node* next;
 
-    Node(int val) {
+    Node(int val) 
+    {
         this->val = val;
         this->next = NULL;
     }
 };
 
-int main() {
+int main()
+{
     Node a(10), b(20), c(30);
 
     a.next = &b;
     b.next = &c;
 
+    // cout << a.val << " " << b.val << " " << c.val << endl;
     cout << a.val << endl;
+    // cout << (*a.next).val << endl;
     cout << a.next->val << endl;
+    // cout << c.val << endl;
     cout << a.next->next->val << endl;
-
+    
     return 0;
 }
