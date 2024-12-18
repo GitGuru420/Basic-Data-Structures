958bed3e0b198303a3b8615fe964b6178f121530
GitGuru420 <raisulislam1892@gmail.com>
2024-12-19 00:03:56 +0530

    Linked List Reverse Print

---
+++ Week-02 (Linked List)/Module-6 (Singly Linked List Operations)/Lecture/g_reverse_print_linked_list.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+    public:
+        int value;
+        Node* next;
+    Node(int value)
+    {
+        this->value = value;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node* &head, Node* &tail, int value)
+{
+    Node* newNode = new Node(value);
+    if(head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    
+    // right now temp is last node
+    tail->next = newNode;
+    tail = tail->next;
+}
+
+void print_linked_list(Node* head)
+{
+    Node* temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->value << endl;
+        temp = temp->next;
+    }
+}
+
+void print_reverse(Node* temp) 
+{
+    // base case
+    if(temp == NULL)
+    {
+        return;
+    }
+    print_reverse(temp->next);
+    cout << temp->value << endl;
+}
+
+int main()
+{
+    Node* head = NULL;
+    Node* tail = NULL;
+
+    int val;
+    while(true)
+    {
+        cin >> val;
+        if(val == -1) 
+        {
+            break;
+        }
+        insert_at_tail(head, tail, val);
+    }
+    print_reverse(head);
+
+    return 0;
+}
