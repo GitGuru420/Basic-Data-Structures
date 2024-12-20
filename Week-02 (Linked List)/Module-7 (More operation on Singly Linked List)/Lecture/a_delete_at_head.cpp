e0d0abc3bb3ee3483a95b98e6d1074f6285cd4b7
GitGuru420 <raisulislam1892@gmail.com>
2024-12-20 18:39:51 +0530

    Delete at head

---
+++ Week-02 (Linked List)/Module-7 (More operation on Singly Linked List)/Lecture/a_delete_at_head.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+    public:
+        int value;
+        Node* next;
+    Node(int value)
+    {
+        this->value = value;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node* &head, Node* &tail, int value)
+{
+    Node* newNode = new Node(value);
+    if(head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    
+    // right now temp is last node
+    tail->next = newNode;
+    tail = tail->next;
+}
+
+void print_linked_list(Node* head)
+{
+    Node* temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->value << endl;
+        temp = temp->next;
+    }
+}
+
+void delete_at_head(Node* &head)
+{
+    Node* deleteNode = head;
+    head = head->next;
+    delete deleteNode;
+}
+
+int main()
+{
+    Node* head = NULL;
+    Node* tail = NULL;
+
+    int val;
+    while(true)
+    {
+        cin >> val;
+        if(val == -1) 
+        {
+            break;
+        }
+        insert_at_tail(head, tail, val);
+    }
+    delete_at_head(head);
+    print_linked_list(head);
+
+    return 0;
+}
