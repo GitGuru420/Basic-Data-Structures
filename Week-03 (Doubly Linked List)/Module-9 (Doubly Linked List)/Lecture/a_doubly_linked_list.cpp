43dff74ff8c5da26c7a40bb8a9df01d84ba9b6d6
GitGuru420 <raisulislam1892@gmail.com>
2024-12-23 00:33:47 +0600

    Creation of doubly linked list

---
+++ Week-03 (Doubly Linked List)/Module-9 (Doubly Linked List)/Lecture/a_doubly_linked_list.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+    public:
+        Node* previous;
+        int value;
+        Node* next;
+    Node(int value)
+    {
+        this->previous = NULL;
+        this->value = value;
+        this->next = NULL;
+    }
+};
+
+int main()
+{
+    // node creation
+    Node* head = new Node(10);
+    Node* a = new Node(20);
+    Node* tail = new Node(30);
+
+    // node connections
+    head->next = a;
+    a->previous = head;
+
+    a->next = tail;
+    tail->previous = a;
+
+    return 0;
+}
