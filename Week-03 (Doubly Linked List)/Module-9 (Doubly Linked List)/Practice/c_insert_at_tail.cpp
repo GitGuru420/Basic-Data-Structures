0000029b0cc85df39014baf1393f9bd4050eede7
GitGuru420 <raisulislam1892@gmail.com>
2024-12-24 19:13:23 +0600

    Insert at tail

---
+++ Week-03 (Doubly Linked List)/Module-9 (Doubly Linked List)/Practice/c_insert_at_tail.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+    public:
+        Node* previous;
+        int value;
+        Node* next;
+    Node(int value)
+    {
+        this->previous = NULL;
+        this->value = value;
+        this->next = NULL;
+    }
+};
+
+// print doubly linked list forward
+void print_forward(Node* &head)
+{
+    Node* temp = head;
+    while(temp != NULL)
+    {
+        cout << temp->value << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+void insert_at_tail(Node* &head, Node* &tail, int value)
+{
+    Node* newNode = new Node(value);
+    if(head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next = newNode;
+    newNode->previous = tail;
+    tail = newNode;
+}
+
+int main()
+{
+    // node creation
+    Node* head = new Node(10);
+    Node* a = new Node(20);
+    Node* tail = new Node(30);
+
+    // link creation
+    head->next = a;
+    a->previous = head;
+
+    a->next = tail;
+    tail->previous = a;
+
+    // insert at head
+    insert_at_tail(head, tail, 40);
+
+    // print function call
+    print_forward(head);
+    
+    return 0;
+}
