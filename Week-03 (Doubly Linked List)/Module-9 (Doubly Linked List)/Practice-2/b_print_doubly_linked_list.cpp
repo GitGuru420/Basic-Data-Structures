4db04e3f99f5a638e65c238bf467f760de634ef5
GitGuru420 <raisulislam1892@gmail.com>
2024-12-25 13:39:34 +0600

    Doubly linked list print

---
+++ Week-03 (Doubly Linked List)/Module-9 (Doubly Linked List)/Practice-2/b_print_doubly_linked_list.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+    public:
+        Node* previous;
+        int value;
+        Node* next;
+    Node(int value)
+    {
+        this->previous = NULL;
+        this->value = value;
+        this->next = NULL;
+    }
+};
+
+// print doubly linked list forward
+void print_forward(Node* head)
+{
+    Node* temp = head;
+    while(temp != NULL)
+    {
+        cout << temp->value << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    // node creation
+    Node* head = new Node(10);
+    Node* a = new Node(20);
+    Node* tail = new Node(30);
+
+    // link connection
+    head->next = a;
+    a->previous = head;
+    
+    a->next = tail;
+    tail->previous = a;
+
+    // print doubly linked list
+    print_forward(head);
+
+    return 0;
+}
