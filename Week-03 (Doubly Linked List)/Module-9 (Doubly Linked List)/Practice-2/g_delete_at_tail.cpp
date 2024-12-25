86f79c1c5bb62059d64a8b586d127b4b3d191642
GitGuru420 <raisulislam1892@gmail.com>
2024-12-25 14:43:59 +0600

    Delete at tail

---
+++ Week-03 (Doubly Linked List)/Module-9 (Doubly Linked List)/Practice-2/g_delete_at_tail.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+    public:
+        Node* previous;
+        int value;
+        Node* next;
+    Node(int value)
+    {
+        this->previous = NULL;
+        this->value = value;
+        this->next = NULL;
+    }
+};
+
+// print doubly linked list forward
+void print_forward(Node* head)
+{
+    Node* temp = head;
+    while(temp != NULL)
+    {
+        cout << temp->value << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+// delete at tail
+void delete_at_tail(Node* &head, Node* &tail)
+{
+    Node* deleteNode = tail;
+    if(tail == NULL)
+    {
+        head = NULL;
+        return;
+    }
+    tail = tail->previous;
+    delete deleteNode;
+    tail->next = NULL;
+}
+
+int main()
+{
+    // node creation
+    Node* head = new Node(10);
+    Node* a = new Node(20);
+    Node* tail = new Node(30);
+
+    // link connection
+    head->next = a;
+    a->previous = head;
+    
+    a->next = tail;
+    tail->previous = a;
+
+    // Node* head = NULL;
+    // Node* tail = NULL;
+    // delete at tail
+    delete_at_tail(head, tail);
+
+    // print doubly linked list
+    print_forward(head);
+
+    return 0;
+}
