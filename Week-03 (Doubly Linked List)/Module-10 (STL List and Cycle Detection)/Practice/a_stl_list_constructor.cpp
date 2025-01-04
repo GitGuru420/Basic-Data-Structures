14c6cc042f6444f7e845bd28c961de4171fa6f06
GitGuru420 <raisulislam1892@gmail.com>
2025-01-04 18:56:59 +0600

    STL list constructor

--- Week-03 (Doubly Linked List)/Module-10 (STL List and Cycle Detection)/Practice/a_stl_list_constructor.cpp
+++ Week-03 (Doubly Linked List)/Module-10 (STL List and Cycle Detection)/Practice/a_stl_list_constructor.cpp
@@ -3,56 +3,62 @@ using namespace std;
 
 int main()
 {
-    // way-1: TC -> O(1)
-    // list<int> lst;    // constructs a list with 0 elements
-    // cout << lst.size() << endl;
+    // way-1
+    // list<int> l;    
+    // cout << "List size: " << l.size() << endl;
+    // cout << "List value: " << *l.begin() << endl;
 
-    // way-2: TC -> O(N)
-    // list<int> lst(5);   // constructs a list with 5 elements and the value will be garbage
-    // cout << lst.size() << endl;
-
-    // print way-1
-    // for(int val : lst)
+    // way-2
+    // list<int> l(5);
+    // cout << "List size: " << l.size() << endl;
+    // for(int val : l)
     // {
     //     cout << val << " ";
     // }
-
-    // print way-2
-    // for(auto it = lst.begin(); it != lst.end(); it++)
-    // {
-    //     cout << *it << " ";
-    // }
-
-    // way-3: TC -> O(N)
-    // list<int> lst(5, 2);    // constructs a list with 5 elements and the value will be 2
-    // for(int val : lst)
+    // cout << endl;
+    
+    // way-3
+    // list<int> l(5, 10);
+    // for(int val : l)
     // {
     //     cout << val << " ";
     // }
+    // cout << endl;
 
-    // way-4: TC -> O(N)
-    // list<int> lst = {1, 2, 3, 4, 5};
-    // list<int> lst2(lst);    // constructs a list by coping lst to lst2
-    // for(int val : lst2)
+    // way-4
+    // list<int> l(5, 12);
+    // list<int> l2(l);
+    // for(int val : l2)
     // {
     //     cout << val << " ";
     // }
+    // cout << endl;
 
-    // way-5: TC -> O(N)
-    // int arr[] = {10, 20, 30, 40, 50};
-    // list<int> lst(arr, arr+5);
-    // for(int val : lst)
+    // way-5
+    // int a[] = {1, 2, 3, 4, 5};
+    // list<int> l(a, a+5);
+    // for(int val : l)
     // {
     //     cout << val << " ";
     // }
+    // cout << endl;
 
-    // way-6: TC -> O(N)
-    vector<int> v = {100, 200, 300};
-    list<int> lst(v.begin(), v.end());
-    for(int val : lst)
+    // way-6
+    // vector<int> v = {1, 2, 3, 4, 5};
+    // list<int> l(v.begin(), v.end());
+    // for(int val : l)
+    // {
+    //     cout << val << " ";
+    // }
+    // cout << endl;
+
+    // another way to print list
+    list<int> l = {100, 20, 30, 40, 50};
+    for(auto it = l.begin(); it != l.end(); it++)
     {
-        cout << val << " ";
+        cout << *it << " ";
     }
+    cout << endl;
 
     return 0;
 }
