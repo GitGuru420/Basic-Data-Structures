e69c9196d1a25df027c19f6f055f79f2c9e67dca
GitGuru420 <raisulislam1892@gmail.com>
2025-01-05 19:44:18 +0600

    Check doubly linked list palindrome or not?

---
+++ Week-03 (Doubly Linked List)/Module-10.5 (Practice Day-1)/P3.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+    public:
+        Node* previous;
+        int value;
+        Node* next;
+    Node(int value)
+    {
+        this->previous = NULL;
+        this->value = value;
+        this->next = NULL;
+    }
+};
+
+// insert at tail
+void insert_at_tail(Node* &head, Node* &tail, int value)
+{
+    Node* newNode = new Node(value);
+    if(head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next = newNode;
+    newNode->previous = tail;
+    tail = newNode;
+}
+
+// print forward doubly linked list
+void print_forward(Node* head)
+{
+    Node* temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->value << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+// check palindrome
+void check_palindrome(Node* head, Node* tail)
+{
+    bool flag = true;
+    for(Node *i = head, *j = tail; i != j && i->previous != j; i = i->next, j = j->previous)
+    {
+        if(i->value != j->value)
+        {
+            flag = false;
+            break;
+        }
+    }
+    if(flag == true)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+}
+
+int main()
+{
+    Node* head = NULL;
+    Node* tail = NULL;
+
+    int value;
+    while(true)
+    {
+        cin >> value;
+        if(value == -1)
+        {
+            break;
+        }
+        insert_at_tail(head, tail, value);
+    }
+
+    check_palindrome(head, tail);
+
+    return 0;
+}
