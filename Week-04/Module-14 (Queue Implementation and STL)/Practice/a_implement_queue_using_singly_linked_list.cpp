d5aad19811de2c24c20fd546fa523b5afdcf77f6
GitGuru420 <raisulislam1892@gmail.com>
2025-01-09 23:17:07 +0600

    Implement queue using singly linked list

---
+++ Week-04/Module-14 (Queue Implementation and STL)/Practice/a_implement_queue_using_singly_linked_list.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+    public:
+        int value;
+        Node* next;
+    Node(int value)
+    {
+        this->value = value;
+        this->next = NULL;
+    }
+};
+
+class myQueue
+{
+    public:
+        Node* head = NULL;
+        Node* tail = NULL;
+        int sz = 0;
+    
+    void push(int value)
+    {
+        sz++;
+        Node* newNode = new Node(value);
+        if(head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+        tail->next = newNode;
+        tail = newNode;
+    }
+    void pop()
+    {
+        sz--;
+        Node* deleteNode = head;
+        head = head->next;
+        delete deleteNode;
+        if(head == NULL)
+        {
+            tail = NULL;
+        }
+    }
+    int front()
+    {
+        return head->value;
+    }
+    int back()
+    {
+        return tail->value;
+    }
+    int size()
+    {
+        return sz;
+    }
+    bool empty()
+    {
+        return head == NULL;
+    }
+};
+
+int main()
+{
+    myQueue q;
+    int n;
+    cin >> n;
+    while(n--)
+    {
+        int val;
+        cin >> val;
+        q.push(val);
+    }
+
+    while(!q.empty())
+    {
+        cout << q.front() << " ";
+        q.pop();
+    }
+    
+    return 0;
+}
