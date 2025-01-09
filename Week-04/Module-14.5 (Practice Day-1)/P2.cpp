c2bf73e6c0ad2999aff5c165e88fd14aae6328da
GitGuru420 <raisulislam1892@gmail.com>
2025-01-10 00:31:51 +0600

    Check a stack and a queue are same or not

---
+++ Week-04/Module-14.5 (Practice Day-1)/P2.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{   
+    stack<int> st, st2;
+    queue<int> q, q2;
+
+    int n;
+    cin >> n;
+    while(n--)
+    {
+        int val;
+        cin >> val;
+        st.push(val);
+    }
+
+    int m;
+    cin >> m;
+    while(m--)
+    {
+        int val;
+        cin >> val;
+        q.push(val);
+    }
+    
+    if(n != m)
+    {
+        cout << "NO" << endl;
+    }
+    else
+    {
+        bool flag = true;
+        while(!st.empty())
+        {
+            if(st.top() != q.front())
+            {
+                flag = false;
+            }
+            st.pop();
+            q.pop();
+        }
+        if(flag == true)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    
+    return 0;
+}
