cf589be2589fed8be33866590957ad62280b6402
GitGuru420 <raisulislam1892@gmail.com>
2025-01-12 18:38:22 +0600

    Printing binary tree by post order traversal

---
+++ Week-05/Module-17 (Binary Tree)/Lecture/d_printing_binary_tree_postorder_traversal.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *left;
+    Node *right;
+    Node(int value)
+    {
+        this->value = value;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+void postorder(Node* root)
+{
+    if(root == NULL)
+        return;
+    postorder(root->left);    // left  
+    postorder(root->right);   // right 
+    cout << root->value << " ";     // root 
+}
+
+int main()
+{
+    // node creation
+    Node *root = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *c = new Node(40);
+    Node *d = new Node(50);
+    Node *e = new Node(60);
+
+    // node connection
+    root->left = a;
+    root->right = b;
+    a->left = c;
+    b->left = d;
+    b->right = e;
+
+    postorder(root);
+
+    return 0;
+}
