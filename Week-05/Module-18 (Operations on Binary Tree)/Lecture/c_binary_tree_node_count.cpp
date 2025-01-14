28e4e5db8d9aec64d16fc9ef8ae30f3515065c3c
GitGuru420 <raisulislam1892@gmail.com>
2025-01-14 20:26:59 +0600

    Count nodes in a binary tree

---
+++ Week-05/Module-18 (Operations on Binary Tree)/Lecture/c_binary_tree_node_count.cpp
@@ -0,0 +1,84 @@
+/*
+            10
+        20          30
+    40          50      60
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *left;
+    Node *right;
+    Node(int value)
+    {
+        this->value = value;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node* input_tree()
+{
+    int val;
+    cin >> val;
+    Node *root;
+    if(val == -1)
+        root = NULL;
+    else
+        root = new Node(val);
+    queue<Node *> q;
+    // if(root != NULL)
+    if(root)
+        q.push(root);
+
+    while (!q.empty())
+    {
+        // 1. ber kore ana
+        Node *p = q.front(); // p->parent
+        q.pop();
+
+        // 2. oii node niye kaj
+        int l, r;
+        cin >> l >> r;
+        Node *myLeft, *myRight;
+        if (l == -1)
+            myLeft = NULL;
+        else
+            myLeft = new Node(l);
+
+        if (r == -1)
+            myRight = NULL;
+        else
+            myRight = new Node(r);
+
+        p->left = myLeft;
+        p->right = myRight;
+
+        // 3. children push kora
+        if (p->left)
+            q.push(p->left);
+        if (p->right)
+            q.push(p->right);
+    }
+    return root;
+}
+
+int count_nodes(Node* root)
+{
+    if(root == NULL)
+        return 0;
+    int l = count_nodes(root->left);
+    int r = count_nodes(root->right);
+    return l+r+1;
+}
+
+int main()
+{
+    Node *root = input_tree();
+    cout << count_nodes(root) << endl;
+    return 0;
+}
