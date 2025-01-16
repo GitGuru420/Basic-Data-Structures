282b01d6128e943204f08832950cc65fd3d6e876
GitGuru420 <raisulislam1892@gmail.com>
2025-01-16 19:27:00 +0600

    Print binary tree by pre order traversal

---
+++ Week-05 (Binary Tree)/Module-17 (Binary Tree)/Practice-2/b_print_binary_tree.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+// pre order traversal : root->left->right
+void pre_order(Node *root)
+{
+    if(root == NULL)
+        return;
+    cout << root->val << " ";
+    pre_order(root->left);
+    pre_order(root->right);
+}
+
+int
+main()
+{
+    // Node creation
+    Node *root = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *c = new Node(40);
+    Node *d = new Node(50);
+    Node *e = new Node(60);
+    Node *f = new Node(70);
+    Node *g = new Node(80);
+    Node *h = new Node(90);
+
+    // Node connection
+    root->left = a;
+    root->right = b;
+
+    a->left = c;
+    a->right = d;
+
+    b->left = e;
+    b->right = f;
+
+    d->left = g;
+    e->right = h;
+
+    // pre order traversal
+    pre_order(root);
+
+    return 0;
+}
