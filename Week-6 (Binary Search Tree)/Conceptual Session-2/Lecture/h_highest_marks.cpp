8e1fc50718a942725f62e4ceed665d3871f4e594
GitGuru420 <raisulislam1892@gmail.com>
2025-01-25 22:20:33 +0600

    Highest Marks

--- Week-6 (Binary Search Tree)/Conceptual Session-2/Lecture/h_highest_marks.cpp
+++ Week-6 (Binary Search Tree)/Conceptual Session-2/Lecture/h_highest_marks.cpp
@@ -21,64 +21,46 @@ class comparator
 public:
     bool operator()(Student a, Student b)
     {
-        if(a.marks == b.marks)
+        if (a.marks == b.marks)
         {
-            if(a.roll  > b.roll)
+            if (a.roll > b.roll)
+            {
                 return true;
+            }
             else
                 return false;
         }
         else
         {
-            if(a.marks < b.marks)
+            if (a.marks < b.marks)
+            {
                 return true;
+            }
             else
                 return false;
         }
     }
 };
 
-void give_current_max(priority_queue<Student, vector<Student>, comparator> &pq)
-{
-    if(!pq.empty())
-    {
-        Student pr = pq.top();
-        cout << pr.name << " " << pr.roll << " " << pr.marks << endl;
-    }  
-    else
-    {
-        cout << "Empty" << endl;
-    }
-}
-
 int main()
 {
+    // default
     priority_queue<Student, vector<Student>, comparator> pq;
 
-    int n; cin >> n;
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < 5; i++)
     {
-        string name; cin >> name;
-        int roll, marks; cin >> roll >> marks;
+        string name;
+        cin >> name;
+        int roll, marks;
+        cin >> roll >> marks;
+
         pq.push(Student(name, roll, marks));
     }
 
-    int q;
-    cin >> q;
-    while(q--)
+    while (!pq.empty())
     {
-        int x; cin >> x;
-        if(x == 0)
-        {
-            string name; cin >> name;
-            int roll, marks; cin >> roll >> marks;
-            pq.push(Student(name, roll, marks));
-        }
-        else if(x == 2)
-        {
-            if(!pq.empty())
-                pq.pop();
-        }
-        give_current_max(pq);
+        Student pr = pq.top();
+        cout << pr.roll << " " << pr.name << " " << pr.marks << endl;
+        pq.pop();
     }
 }
