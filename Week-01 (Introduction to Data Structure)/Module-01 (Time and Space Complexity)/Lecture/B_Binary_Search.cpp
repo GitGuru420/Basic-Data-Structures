41039118a16a8cace5ea250b6ac681513e091bc8
GitGuru420 <raisulislam1892@gmail.com>
2025-01-30 00:48:52 +0600

    CF - Binary Search (TLE)

--- Week-01 (Introduction to Data Structure)/Module-01 (Time and Space Complexity)/Lecture/B_Binary_Search.cpp
+++ Week-01 (Introduction to Data Structure)/Module-01 (Time and Space Complexity)/Lecture/B_Binary_Search.cpp
@@ -1,29 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    int n, q;
-    cin >> n >> q;
-    int a[n];
+int main()
+{
+    int n, q; cin >> n >> q;
+    int arr[n];
     for(int i=0; i<n; i++) {
-        cin >> a[i];
+        cin >> arr[i];
     }
-    for(int i=1; i<=q; i++) {
-        int x;
-        cin >> x;
+    for(int i=0; i<q; i++) {
+        int x; cin >> x;
         int flag = 0;
         for(int i=0; i<n; i++) {
-            if(a[i] == x) {
+            if(arr[i] == x) 
                 flag = 1;
-            }
         }
-        if(flag == 1) {
+        if(flag == 1) 
             cout << "found" << endl;
-        }
-        else {
+        else 
             cout << "not found" << endl;
-        }
     }
+
+    // This code give TLE (Time Limit Exceeded)
 
     return 0;
 }
